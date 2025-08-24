474a35e64346b646ebdf1414d7a961561b181799
Teena <123teenakannan@gmail.com>
2025-08-25 00:01:16 +0800

    Added GpuCommandPool data and functionality CreateGpuCommandPool, ResetGpuCommandPool, DestroyGpuCommandPool

---
+++ Source/RHI/CommandPool.cpp
@@ -0,0 +1,33 @@
+#include "RHI/CommandPool.h"
+
+#include "RHI/CommonHelper.h"
+#include "RHI/Device.h"
+#include "volk.h"
+
+gran::GpuCommandPool gran::RHI::CommandPool::CreateGpuCommandPool(
+    const gran::Device& device,
+    const gran::GpuCommandPoolCreateInfo& createInfo)
+{
+	VkCommandPool commandPool = 0;
+	VK_CHECK(vkCreateCommandPool(
+	    device.m_Device,
+	    &createInfo.m_CreateInfo,
+	    nullptr,
+	    &commandPool));
+
+	return { commandPool };
+}
+
+void gran::RHI::CommandPool::ResetGpuCommandPool(
+    const gran::Device& device,
+    gran::GpuCommandPool& pool)
+{
+	VK_CHECK(vkResetCommandPool(device.m_Device, pool.m_CommandPool, 0));
+}
+
+void gran::RHI::CommandPool::DestroyGpuCommandPool(
+    const gran::Device& device,
+    gran::GpuCommandPool& pool)
+{
+	vkDestroyCommandPool(device.m_Device, pool.m_CommandPool, nullptr);
+}
---
+++ Source/RHI/CommandPool.h
@@ -0,0 +1,39 @@
+#include "vulkan/vulkan.h"
+
+// Forward declaration
+namespace gran
+{
+	struct Device;
+	struct GpuAllocator;
+} // namespace gran
+
+namespace gran
+{
+	struct GpuCommandPoolCreateInfo
+	{
+		VkCommandPoolCreateInfo m_CreateInfo {
+			.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO,
+			//.flags = VK_COMMAND_POOL_CREATE_TRANSIENT_BIT,
+			//.flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT,
+			//.queueFamilyIndex
+		};
+	};
+
+	struct GpuCommandPool
+	{
+		VkCommandPool m_CommandPool;
+	};
+
+} // namespace gran
+
+namespace gran::RHI::CommandPool
+{
+	GpuCommandPool CreateGpuCommandPool(
+	    const gran::Device& device,
+	    const gran::GpuCommandPoolCreateInfo& createInfo);
+
+	void ResetGpuCommandPool(const gran::Device& device, GpuCommandPool& pool);
+
+	void DestroyGpuCommandPool(const gran::Device& device, GpuCommandPool& pool);
+
+} // namespace gran::RHI::CommandPool
